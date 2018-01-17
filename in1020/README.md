131858375e595df3428ef68923f3b43c46a7222c
ken.hurtado <ken.hurtado@icarus.com>
2018-01-17 00:06:22 -0700

    Added Noding Bat's in1020

---
+++ in1020/README.md
@@ -0,0 +1,6 @@
+# in1020
+
+http://nodingbat.com/exercises/warmup-1/in1020
+
+
+Given 2 int values, return true if either of them is in the range 10..20 inclusive.
---
+++ in1020/index.js
@@ -0,0 +1,8 @@
+var in1020=function(a, b) { 
+    if ((a >= 10 && a <= 20) || (b >= 10 && b <= 20)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+console.log(in1020(10, 5));
