d2cf6a479984251dc8ce1ee2d68326c163bd8e36
ken.hurtado <ken.hurtado@icarus.com>
2018-01-18 09:24:30 -0700

    Fixed Noding Bat's arrayCount9

--- arrayCount9/index.js
+++ arrayCount9/index.js
@@ -1,7 +1,10 @@
 var arrayCount9 = function(nums){
     let nines = 0;
     for (let i = 0; i < nums.length; i++) {
-        return nums[i]
+        if (nums[i] == '9') {
+            nines++;
+        }
     }
+    return nines;
 }
-console.log(arrayCount9[1,9,3,9]);
+console.log(arrayCount9([1,3,9,3,9]));
