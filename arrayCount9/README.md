d83b75a7c631d0d7ca488374ff26fdcf25ea7a5c
ken.hurtado <ken.hurtado@icarus.com>
2018-01-18 09:07:16 -0700

    Added Noding Bat's arrayCount9, but didn't finish it.

---
+++ arrayCount9/README.md
@@ -0,0 +1,6 @@
+# arrayCount9
+
+http://nodingbat.com/exercises/warm-up2/arrayCount9
+
+
+Given an array of ints, return the number of 9's in the array.
---
+++ arrayCount9/index.js
@@ -0,0 +1,7 @@
+var arrayCount9 = function(nums){
+    let nines = 0;
+    for (let i = 0; i < nums.length; i++) {
+        return nums[i]
+    }
+}
+console.log(arrayCount9[1,9,3,9]);
