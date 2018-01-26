123e5b7a5ffa3ed60212cc348c35c16e080edb30
ken.hurtado <khurtado@protonmail.com>
2018-01-26 12:06:18 -0700

    Added Noding Bat's startHi

---
+++ startHi/README.md
@@ -0,0 +1,6 @@
+# NodingBat
+
+http://nodingbat.com/exercises/warm-up1/startHi
+
+Given a string, return true if the string starts with 'hi'
+and false otherwise.
---
+++ startHi/index.js
@@ -0,0 +1,7 @@
+var startHi = function(str){ 
+	if(str.charAt(0) == 'h' && str.charAt(1) == 'i') {
+      return true;
+    } else {
+      return false
+    }
+}
