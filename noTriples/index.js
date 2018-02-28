b053fb6ce244332ab8151d25698fdf6568f56f1e
ken.hurtado <khurtado@protonmail.com>
2018-02-27 22:11:52 -0700

    Finished noTriples.

--- noTriples/index.js
+++ noTriples/index.js
@@ -1,22 +1,11 @@
 var noTriples=function(nums) { 
-    if (nums.length < 3) {
-        return true
-    }
-    for(let i = 0; i < nums.length; i++) {
-        if ((nums[i] == nums[i + 1]) && (nums[i + 1] == nums[i + 2])) {
-            return false;
-        } else {
-            return true
-        }
-    }
-    // for(let i = 0; i < nums.length; i ++) {
-    //     for(let j = 0; j < i; j++) {
-    //         if((nums[j] == nums[j + 1]) && (nums[j + 1] == nums[j + 2])) {
-    //             return false;
-    //         } else {
-    //             return true;
-    //         }
-    //     }
-    // }
+    let count = 0;
+    for (let i = 0 ; i < nums.length - 1; i++) {
+      if (i+2 <= nums.length -1) {
+        if (nums[i] == nums[i+1] && nums[i] == nums[i+2])
+          return false;  
+      }
+    } 
+    return true;
 }
 console.log(noTriples([2,2,2,2,1]))
