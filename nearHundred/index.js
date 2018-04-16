a5293c8fea55085cb7c3fe5aa8eb442ac405bfa4
ken.hurtado <khurtado@protonmail.com>
2018-04-16 13:42:21 -0600

    Completed NodingBat nearHundred.

---
+++ nearHundred/index.js
@@ -0,0 +1,11 @@
+nearHundred = function(n){ 
+    if ((n + 10 >= 100) && (n - 10 <= 100)) {
+        return true;
+    } else if ((n + 10 >= 200) && (n - 10 <= 200)) {
+        return true;
+    } else {
+        return false
+    }
+}
+
+console.log(nearHundred(99));
